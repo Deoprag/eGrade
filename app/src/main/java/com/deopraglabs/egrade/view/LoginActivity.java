682ef6f83372c51495a54a1de68dafd8f3e683a0
Pedro Rocha <pdroesofiarabelo@gmail.com>
2024-03-13 18:20:08 -0300

    Alterações na tela inicial - Foi alterado a tela inicial do sistema. Trocado por uma animação

---
+++ app/src/main/java/com/deopraglabs/egrade/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.deopraglabs.egrade.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.deopraglabs.egrade.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/deopraglabs/egrade/view/MainActivity.java
+++ app/src/main/java/com/deopraglabs/egrade/view/MainActivity.java
@@ -1,12 +1,24 @@
 package com.deopraglabs.egrade.view;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.deopraglabs.egrade.R;
+
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }, 3000);
     }
 }
+
