7afd82afa0cee0bc27f1e573973cd9b466d6673c
Pedro Rabelo <pedro@txfuel.com.br>
2024-07-01 09:14:58 -0300

    m

--- app/src/main/java/com/deopraglabs/egrade/view/ProfessorGradeFragment.java
+++ app/src/main/java/com/deopraglabs/egrade/view/ProfessorGradeFragment.java
@@ -1,22 +1,21 @@
 package com.deopraglabs.egrade.view;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.deopraglabs.egrade.R;
 import com.deopraglabs.egrade.model.Professor;
 
-public class ProfessorCourseFragment extends Fragment {
+public class ProfessorGradeFragment extends Fragment {
 
     private Professor professor;
 
-    public static ProfessorCourseFragment newInstance(Professor professor) {
-        ProfessorCourseFragment fragment = new ProfessorCourseFragment();
+    public static ProfessorGradeFragment newInstance(Professor professor) {
+        ProfessorGradeFragment fragment = new ProfessorGradeFragment();
         Bundle args = new Bundle();
 
         fragment.setArguments(args);
@@ -34,6 +33,6 @@ public class ProfessorCourseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_professor_courses, container, false);
+        return inflater.inflate(R.layout.fragment_professor_grade, container, false);
     }
 }
--- app/src/main/java/com/deopraglabs/egrade/view/StudentHomeFragment.java
+++ app/src/main/java/com/deopraglabs/egrade/view/StudentHomeFragment.java
@@ -1,39 +1,97 @@
-package com.deopraglabs.egrade.view;
+package com.deopraglabs.egrade.view;// StudentHomeFragment.java
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.deopraglabs.egrade.R;
-import com.deopraglabs.egrade.model.Student;
+import com.deopraglabs.egrade.adapter.RankingAdapter;
+import com.deopraglabs.egrade.model.Grade;
+import com.deopraglabs.egrade.model.Subject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StudentHomeFragment extends Fragment {
 
-    private Student student;
+    private RecyclerView rankingRecyclerView;
+    private RankingAdapter rankingAdapter;
+    private List<Grade> gradesList = new ArrayList<>();
+    private List<Subject> subjectsList = new ArrayList<>();
+    private Spinner subjectSpinner;
 
-    public static StudentProfileFragment newInstance(Student student) {
-        StudentProfileFragment fragment = new StudentProfileFragment();
-        Bundle args = new Bundle();
-        args.putSerializable("user", student);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_student_home, container, false);
+
+        rankingRecyclerView = view.findViewById(R.id.rankingRecyclerView);
+        subjectSpinner = view.findViewById(R.id.subjectSpinner);
+
+        populateSubjects();
+        ArrayAdapter<Subject> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, subjectsList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        subjectSpinner.setAdapter(adapter);
+
+        subjectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Subject subject = (Subject) parent.getItemAtPosition(position);
+                updateRanking(subject);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        rankingRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        rankingAdapter = new RankingAdapter(new ArrayList<>());
+        rankingRecyclerView.setAdapter(rankingAdapter);
+
+        updateRanking(null);
+
+        return view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            student = (Student) getArguments().getSerializable("user");
+    private void populateSubjects() {
+        //adicionar no subjectList
+    }
+
+    private void updateRanking(@Nullable Subject subject) {
+        List<Grade> filteredGrades = filterGradesBySubject(subject);
+        rankingAdapter.updateGrades(filteredGrades);
+    }
+
+    private List<Grade> filterGradesBySubject(@Nullable Subject subject) {
+        List<Grade> allGrades = getAllGrades();
+
+        if (subject == null || subject.getName().equals("Todas")) {
+            return allGrades.stream()
+                    .sorted((g1, g2) -> Float.compare(g2.getN1(), g1.getN1())) // Ordena por nota, por exemplo
+                    .limit(5)
+                    .collect(Collectors.toList());
         }
+
+        return allGrades.stream()
+                .filter(grade -> grade.getSubject().getName().equals(subject.getName()))
+                .sorted((g1, g2) -> Float.compare(g2.getN1(), g1.getN1()))
+                .limit(5)
+                .collect(Collectors.toList());
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_student_home, container, false);
+    private List<Grade> getAllGrades() {
+        // Listar grades
+        return null;
     }
 }
